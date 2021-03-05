d3d749c3fbd2efaed828a057177373d7f29612e8
Jacob Salzberg <jssalzbe@ncsu.edu>
2021-03-05 12:32:02 -0500

    Change to ubuntu command name

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export scale=10;
-export line_rate=$(cat coverage/cobertura-coverage.xml | xml fo -D | xml sel -t -m "coverage" -v "@line-rate")
+export line_rate=$(cat coverage/cobertura-coverage.xml | xmlstarlet fo -D | xmlstarlet sel -t -m "coverage" -v "@line-rate")
 export target=0.8
 export passing_p=$(echo $line_rate '>=' '0.8' | bc)
 exec test $passing_p -eq 1
